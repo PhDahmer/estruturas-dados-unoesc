3fd917d6a8ed87847dae6dbeea6cfefcbd18b9a3
ph_dahmer <pedrohdahmer@proton.me>
2025-08-04 19:56:56 -0300

    Atualiza atividade 2

--- Aula 1/Atividade 2.c
+++ Aula 1/Atividade 2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int cmpvint(int a1[], int a2[], int n){
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i <= n; i++) {
         if (a1[i] != a2[i]) {
             return 0; 
         }
