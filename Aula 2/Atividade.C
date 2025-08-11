a52d46b119f32bd1779a10203dd87c23e09f2992
ph_dahmer <pedrohdahmer@proton.me>
2025-08-11 20:06:06 -0300

    Correção da Atividade 2 durante a Aula 11/08/2025

--- Aula 2/Atividade.C
+++ Aula 2/Atividade.C
@@ -1,27 +1,35 @@
 #include <stdio.h>
 
-char* enderposi(char *str, char ch) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == ch) {
-            return &str[i]; 
+char* enderposi(char *pos_ini, char ch) {
+    for (int i = 0; *(pos_ini+i) != '\0'; i++) {
+        if (*(pos_ini+i) == ch) {
+            return pos_ini+i;
         }
     }
     return NULL; 
 }
 
 int main(void) {
-    char str[50], ch;
-    printf("Digite uma string: ");
-    fgets(str, sizeof(str), stdin);
+    char frase[50];
+    char ch, *ptr;
+    int i;  
 
+    printf("Digite uma string: ");
+    scanf("%s", frase);
     printf("Digite um caractere: ");
     scanf(" %c", &ch);
 
-    char *ptr = enderposi(str, ch);
-    if (ptr != NULL) {
-        printf("Caracteres a partir do caractere '%c': %s", ch, ptr);
-    } else {
-        printf("Caractere '%c' nao encontrado na string.\n", ch);
+    ptr = enderposi(frase, ch);
+
+    if (ptr != NULL){
+        for (i=0; *(ptr+i) != '\0'; i++) { // Tudo esse FOR pode ser substituido por printf(ptr)
+            printf("%c", *(ptr+i));
+        }
+        printf("\n");
+    }
+
+    else {
+        printf("O caractere '%c' nao foi encontrado na string.\n", ch);
     }
 
     return 0;
